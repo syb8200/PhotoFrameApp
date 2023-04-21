3c28b3616a2fa6351131db0890d6ee92cb358d16
syb8200 <syb8200@naver.com>
2023-04-21 18:24:13 +0900

    onRequestPermissionsResult 추가 및 SAF 연동

--- app/src/main/java/fastcampus/part1/fc_1_chapter8/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_1_chapter8/MainActivity.kt
@@ -2,9 +2,12 @@ package fastcampus.part1.fc_1_chapter8
 
 import android.Manifest.permission.*
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -12,6 +15,9 @@ import fastcampus.part1.fc_1_chapter8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private val imageLoadLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+        updateImages(uriList)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +44,6 @@ class MainActivity : AppCompatActivity() {
                 requestReadExternalStorage()
             }
         }
-
-        // 권한이 여러개일 경우 arrayOf() 사용한다.
-        ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
     }
 
     private fun showPermissionInfoDialog() {
@@ -54,11 +57,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadImage() {
-        Toast.makeText(this, "이미지를 가져올 예정", Toast.LENGTH_SHORT).show()
+        // 이미지 타입의 모든 파일을 갖고 오겠다. (MIME 타입)
+        imageLoadLauncher.launch("image/*")
     }
 
     private fun requestReadExternalStorage() {
+        // 권한이 여러개일 경우 arrayOf() 사용한다.
+        ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+    }
 
+    private fun updateImages(uriList: List<Uri>) {
+        Log.i("updateImages", "$uriList")
+    }
+
+    // Permissions Allow 누른 다음 다시 버튼 누르지 않도록 편의성 제공
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                    loadImage()
+                }
+            }
+        }
     }
 
     companion object {
